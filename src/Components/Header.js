76072423e61a4239ec5367ec292b93b69155ef47
Aayush Jain <imjainaayush@gmail.com>
2021-06-27 20:55:26 +0530

    github link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,31 @@
-import { Card, CardBody } from "reactstrap";
+import { Card, CardBody, Col, Row } from "reactstrap";
 import React from "react-dom";
+import img from '../img/github.png'
 
 const Header = () => {
     return (
         <div>
             <Card className="my-2 bg-info">
                 <CardBody>
-                    <h1 className="text-center my-2">
-                        Helper on Maps
-                    </h1>
+                    <Row>
+                        <Col md={11}>
+                            <h1 className="text-center my-2">
+                                Helper on Maps
+                            </h1>
+                        </Col>
+                        <Col>
+                            <a
+                                href="https://github.com/straw-hat-coders-defhacks"
+                                target="_blank"
+                            >
+                                <img
+                                    src={img}
+                                    style={{ width: 50, height: 50 }}
+                                    className="my-2"
+                                />
+                            </a>
+                        </Col>
+                    </Row>
                 </CardBody>
             </Card>
         </div>
